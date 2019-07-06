c20b27631c38fd7d537783309de272d1fefbb413
pablitott <4439258+pablitott@users.noreply.github.com>
2019-07-06 11:33:32 -0400

    change to show section2 in course

--- Udemy/nodeJs/basics/simpleServer.js
+++ Udemy/nodeJs/basics/simpleServer.js
@@ -1,14 +1,47 @@
 var http = require('http');
  
-var hostname = 'ec2-18-221-25-10.us-east-2.compute.amazonaws.com';
-var port = 4000;
- 
-var server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
- 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const url = require('url');
+const path=require('path');
+const fs=require('fs');
+const mimeTypes = {
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css"
+};
+http.createServer(function(req, res){
+  var uri = url.parse(req.url).pathname;
+  var fileName = path.join(process.cwd(), unescape(uri));
+  console.log('Loading ' + uri);
+  var stats;
+
+  try{
+    stats = fs.lstatSync(fileName);
+  }catch(e){
+    res.writeHead(404, {'Content-type': 'text/plain'});
+    res.writeHead('404 Not found\n');
+    res.end;
+    return;
+  }
+
+  if(stats.isFile()){
+    var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+    res.writeHead(200, {'Content-type' : mimeType});
+    var fileStream = fs.createReadStream(fileName);
+    fileStream.pipe(res);  
+  }else if(stats.isDirectory()){
+    res.writeHead(302, {
+      'Location': 'inex.html'
+    });
+    res.end;
+  }else{
+    res.writeHead(500, {'Content-type': 'plain/text'});
+    res.write('500 Internal error\n');
+
+  }
+
+}).listen(4000);
+
+
